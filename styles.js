8d7f69e4d39de038d90fb6f58bd7738bfe1d26df
aluochvivian <aluochviviann@gmail.com>
2023-07-08 08:43:49 +0300

    Modify my json

--- styles.js
+++ styles.js
@@ -1,92 +1,98 @@
-//the menu nav bar
+// Menu Navigation
 let navbar = document.querySelector('.header .navbar');
 let menuBtn = document.querySelector('#menu-btn');
 let closeBtn = document.querySelector('#close-navbar');
 
-menuBtn.onclick = () =>{
-   navbar.classList.add('active');
+menuBtn.onclick = () => {
+  navbar.classList.add('active');
 };
 
-closeBtn.onclick = () =>{
-    navbar.classList.remove('active');
- };
-
-window.onscroll = () =>{
-   navbar.classList.remove('active');
+closeBtn.onclick = () => {
+  navbar.classList.remove('active');
 };
-//the menu nav bar
 
-//Styling the book a visit 
+window.onscroll = () => {
+  navbar.classList.remove('active');
+};
 
 // Fetch Pricing Data
 function fetchPricingData() {
-   return new Promise((resolve, reject) => {
-     // Simulating an asynchronous API call
-     setTimeout(() => {
-       // Resolve with the pricing data
-       resolve(pricingData.pricing);
-     }, 1000); // Simulating 1 second delay
-   });
- }
- 
- // Display Pricing Details
- // Fetch the pricing data from the JSON file
-fetch('pricing.json')
-.then(response => response.json())
-.then(data => {
-  const pricingContainer = document.getElementById('pricing');
-  const pricingData = data.prices;
-
-  // Loop through the pricing data and create HTML elements for each item
-  pricingData.forEach(item => {
-    const box = document.createElement('div');
-    box.classList.add('box');
-
-    const title = document.createElement('h3');
-    title.classList.add('title');
-    title.textContent = item.title;
-
-    const price = document.createElement('div');
-    price.classList.add('price');
-
-    const currency = document.createElement('span');
-    currency.classList.add('currency');
-    currency.textContent = 'ksh';
-
-    const amount = document.createElement('span');
-    amount.classList.add('amount');
-    amount.textContent = item.price;
-
-    price.appendChild(currency);
-    price.appendChild(amount);
-
-    const features = document.createElement('ul');
-    item.features.forEach(feature => {
-      const listItem = document.createElement('li');
-      const icon = document.createElement('i');
-      icon.classList.add('fas', 'fa-check');
-      listItem.appendChild(icon);
-      listItem.appendChild(document.createTextNode(feature));
-      features.appendChild(listItem);
-    });
-
-    const image = document.createElement('img');
-    image.src = item.image;
-    image.alt = item.title;
-
-    const button = document.createElement('button');
-    button.textContent = 'book a visit';
-
-    const link = document.createElement('a');
-    link.href = '#';
-    link.appendChild(button);
-
-    box.appendChild(title);
-    box.appendChild(price);
-    box.appendChild(features);
-    box.appendChild(link);
-
-    pricingContainer.appendChild(box);
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous API call
+    setTimeout(() => {
+      // Resolve with the pricing data
+      resolve(pricingData.prices);
+    }, 1000); // Simulating 1 second delay
   });
-})
-.catch(error => console.log(error));
+}
+
+// Update Capacity
+function updateCapacity(pricingData, index) {
+  pricingData[index]['available-space']--;
+}
+
+// Display Pricing Details
+// Fetch the pricing data from the JSON file
+fetch('db.json')
+  .then((response) => response.json())
+  .then((data) => {
+    const pricingContainer = document.getElementById('pricing');
+
+    data.prices.forEach((item, index) => {
+      const box = document.createElement('div');
+      box.classList.add('box');
+
+      const content = document.createElement('div');
+      content.classList.add('content');
+
+      const title = document.createElement('h3');
+      title.textContent = item.title;
+      content.appendChild(title);
+
+      const price = document.createElement('p');
+      price.textContent = `Price: Ksh ${item.price}`;
+      content.appendChild(price);
+
+      const time = document.createElement('p');
+      time.textContent = `Time: ${item.time}`;
+      content.appendChild(time);
+
+      const capacity = document.createElement('p');
+      capacity.textContent = `Capacity: ${item.capacity}`;
+      content.appendChild(capacity);
+
+      const availableSpace = document.createElement('p');
+      availableSpace.textContent = `Available Space: ${item['available-space']}`;
+      content.appendChild(availableSpace);
+
+      const link = document.createElement('a');
+      link.href = '#visit';
+      link.textContent = 'Book a Visit';
+      link.classList.add('btn');
+
+      // Add event listener to update capacity
+      link.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default link behavior
+        if (item['available-space'] > 0) {
+          updateCapacity(data.prices, index);
+          availableSpace.textContent = `Available Space: ${item['available-space']}`;
+        }
+      });
+
+      content.appendChild(link);
+
+      box.appendChild(content);
+
+      pricingContainer.appendChild(box);
+    });
+  })
+  .catch((error) => console.log(error));
+
+// Scroll to the Pricing section when clicking on the "Book a Visit" link
+const bookVisitLink = document.querySelector('.navbar a[href="#visit"]');
+bookVisitLink.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent the default link behavior
+
+  const pricingSection = document.getElementById('pricing');
+  pricingSection.scrollIntoView({ behavior: 'smooth' });
+});
